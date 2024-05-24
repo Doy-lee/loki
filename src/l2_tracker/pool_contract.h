beab0855dea9c87d163331121a3713de50a88280
doylet <doyle.github.2ifxi@simplelogin.com>
2024-05-24 13:43:02 +1000

    Revert "Merge pull request #1665 from Doy-lee/doyle-ethyl-backup-providers"
    This reverts commit 8191960bdf172b6af644681e8309fb5c68fed49b, reversing
    changes made to cfc19aadf7411a2d47ca54d5a588804d712df0f9.

--- src/l2_tracker/pool_contract.h
+++ src/l2_tracker/pool_contract.h
@@ -15,10 +15,10 @@ class RewardRateResponse {
 
 class PoolContract {
   public:
-    PoolContract(std::string _contractAddress, ethyl::Provider& _provider);
+    PoolContract(std::string _contractAddress, std::shared_ptr<Provider> _provider);
     RewardRateResponse RewardRate(uint64_t timestamp, uint64_t ethereum_block_height);
 
   private:
     std::string contractAddress;
-    ethyl::Provider& provider;
+    std::shared_ptr<Provider> provider;
 };
